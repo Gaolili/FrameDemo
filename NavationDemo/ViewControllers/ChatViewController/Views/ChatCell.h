9f9144fb9a4f93bdd7d8e6889839a87652c974f6
gaolili <1076787210@qq.com>
2015-09-06 16:14:01 +0800

    调整目录

--- NavationDemo/ViewControllers/ChatViewController/Views/ChatCell.h
+++ NavationDemo/ViewControllers/ChatViewController/Views/ChatCell.h
@@ -3,5 +3,5 @@
 #import <UIKit/UIKit.h>
 
 @interface ChatCell : UITableViewCell
-
+@property (nonatomic,assign)BOOL isOther;//是否是别人发过来的
 @end
--- NavationDemo/ViewControllers/ChatViewController/Views/ChatCell.m
+++ NavationDemo/ViewControllers/ChatViewController/Views/ChatCell.m
@@ -1,13 +1,12 @@
  
 
 #import "ChatCell.h"
+#import <Masonry.h>
 
 @interface ChatCell ()
 @property (nonatomic,strong)UIImageView * avatorImg;
 @property (nonatomic,strong)UIImageView * messBg;
 @property (nonatomic,strong)UILabel * messInfo;
-
-
 @end
 
 @implementation ChatCell
@@ -16,12 +15,27 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         [self setUI];
+        [self setLayouts];
     }
     return self;
 }
 
 - (void)setUI{
     _avatorImg = [[UIImageView alloc]init];
+    [self.contentView addSubview:_avatorImg];
     
+    _messBg = [[UIImageView alloc]init];
+    [self.contentView addSubview:_messBg];
+    
+    _messInfo = [[UILabel alloc]init];
+    [self.contentView addSubview:_messInfo];
+}
+
+- (void)setLayouts{
+    if (!_isOther) {
+        [_avatorImg mas_makeConstraints:^(MASConstraintMaker *make) {
+            
+        }];
+    }
 }
 @end

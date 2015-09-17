1f53c543f3deeaf9fc0ccaa161550681d3d27bb1
gaolili <1076787210@qq.com>
2015-09-17 15:36:04 +0800

    refresh

---
+++ NavationDemo/ViewControllers/TestViewController/Two/AnimationViewController.m
@@ -0,0 +1,25 @@
+ 
+
+#import "AnimationViewController.h"
+
+@interface AnimationViewController ()
+
+@end
+
+@implementation AnimationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self addTitle:@"动画"];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
--- NavationDemo/ViewControllers/TestViewController/Two/SecondViewController.m
+++ NavationDemo/ViewControllers/TestViewController/Two/SecondViewController.m
@@ -3,9 +3,10 @@
 #import "SecondViewController.h"
 #import "LoopView.h"
 #import "PuperPopView.h"
-
+#import "AnimationViewController.h"
 @interface SecondViewController ()<PuperPopViewDelegate>
 @property (nonatomic,strong)PuperPopView * pop;
+@property (nonatomic,strong)LoopView * loopView;
 @end
 
 @implementation SecondViewController
@@ -14,19 +15,25 @@
     self.view.backgroundColor = [UIColor whiteColor];
     [self addrightItems:@[@"img_1",@"img_2"]];
     
-    LoopView * loopView = [[LoopView alloc]initWithFrame:CGRectMake(50, 100, 200, 40)];
-    loopView.context = @"滚动啦";
-    loopView.speed = 0.23;
-    loopView.direction = LVDirectionLeft;
-    [loopView star];
-    [self.view addSubview:loopView];
+    UIImage * image = [UIImage imageNamed:@"intro_1"];
+    UIImageView * imageView = [[UIImageView alloc]initWithImage:image];
+    imageView.frame = CGRectMake(100, -160, 0, 0);
+    
+       [UIView animateWithDuration:1 delay:0.5 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+         imageView.frame = CGRectMake(100, 100, 160, 160);
+        [self.view addSubview:imageView];
+    } completion:^(BOOL finished){
+        
+    }];
+
+
 }
 
 - (void)rightItemAction:(UIButton *)btn{
     if (btn.tag == 100) {
         [self setPopView];
      }else if (btn.tag == 101){
-        NSLog(@"2");
+         [self setLoopView];
     }
 }
 
@@ -36,21 +43,34 @@
          _pop.delegate = self;
          [self.view addSubview:_pop];
     }
-    
     [_pop showInViews];
-   
    
 }
 
 
+- (void)setLoopView{
+    if (!_loopView) {
+        _loopView = [[LoopView alloc]initWithFrame:CGRectMake(50, 100, 200, 40)];
+        _loopView.context = @"滚动啦";
+        _loopView.speed = 0.23;
+        _loopView.direction = LVDirectionLeft;
+        _loopView.hidden = YES;
+        [_loopView star];
+        [self.view addSubview:_loopView];
+    }
+    _loopView.hidden = !_loopView.hidden;
+}
 
 - (void)popViewClickAction:(NSInteger)row{
     NSLog(@"你点击的是--%d",row);
     [_pop hideViews];
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    [_pop hideViews];
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+     [_pop hideViews];
+    AnimationViewController * animation = [[AnimationViewController alloc]init];
+    [self.navigationController pushViewController:animation animated:YES];
 }
+
 
 @end
